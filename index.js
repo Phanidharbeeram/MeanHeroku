d08514baf420805e5a26ce7b21506fd399cff4bf
Phanidhar Beeram <phanidharbeeram@gmail.com>
2020-04-16 10:09:28 +0530

    april16 trail2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const compression = require('compression')
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(compression())
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/route", api);
 mongoose.connect(
