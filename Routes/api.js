918e74d04635561cc59d4837c59f49a57b0964e0
Phanidhar Beeram <phanidharbeeram@gmail.com>
2020-04-16 11:13:38 +0530

    april16 with zgip

--- Routes/api.js
+++ Routes/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Item = require("../Models/ShoppingList");
-router.get("/", (req, res) => {
+router.get("/",(req, res) => {
   Item.find((err, item) => {
     if (err) {
       console.log(err);
